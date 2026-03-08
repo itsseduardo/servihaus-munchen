5724fd2d3455acb00cbf3fab1c63868be102e65e
itsseduardo <jeasarria05@gmail.com>
2026-03-08 01:32:38 +0100

    imagenes reestablecidas

--- frontend/components/Services.tsx
+++ frontend/components/Services.tsx
@@ -4,21 +4,21 @@ const services = [
     description:
       "Regelmäßige und gründliche Reinigung für private und geschäftliche Räumlichkeiten.",
     image:
-      "",
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuBQQ4uE-O9f-huKFmYk3jAHA8TLOVDyEdN29fp20aMegZGKGZnIT3PN9HGq-KRiHD9D6IxEndJKlDmTUo-Vx4UIMPBdtP5pi83gheB9LZIBRXg2c6pFRQSLsK2qLanCc5x5VbwIvZWoNh9R2JQz4bhqtxULy_kn_vdOmqFDDfw56TpWs1F8H-zd677y5pXovBKAN5E6nokjHOdaJfoCtOHKxCYUjA_Tt2HYAmM-jh908jkH2zV1nNukyJUN5e-UAlEtw_DxGAeeFAg",
   },
   {
     title: "Umzugsservice",
     description:
       "Sicherer und stressfreier Transport Ihrer Möbel und Kartons in Ihr neues Zuhause.",
     image:
-      "",
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuA0N_jF1xln6hey7nik9fLbYOvfSmAOha-VLefECKLQAFCNkGJdkfO-BPjRGgt4O4mPvQ4E-ySmQsieTy-24AoYUeaL38HKgj2mwickfA5AyNeQhfPg14hFU3MrCfyCYZqPgwkmOMwdW_INHkKqzw__dBwhHAlMkUPgkeVFxOoM3g8dxSu66hV8uPjUjE_oK9I7Nuh4B0ewbRnSnbUjYnNt_e6Wt-lVxVHrxocQUTIp9s6C346ClMsPLA7Ih9GcYfD0sG05Oabl6TM",
   },
   {
     title: "Handwerkerservice",
     description:
       "Kompetente Reparaturen, Montagen und Instandhaltung durch qualifizierte Fachkräfte.",
     image:
-      "",
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuDuvuTk7vlzqNtXm73AILbCltYENlbSoNqMLc-eh25qnJZSaK9_G3q2CcvxoA3YCRKM5wCBZe3l1Wwj06Pz1lbLjLZw15rUDHBr78CXHjVV80EFOdYWN8FxFNUwlPIcRbQC_m6CJ8A8rwLI9mg4R6nd_sZ79HsaWNnNFG-LlXp_ss8IZh8BREMtoaBHiNrKrfYpwc1XKIepN_YboHasiFVIvWNB9eHo54BMd5g9X-QVtKK3eZE7dmFsEPxXCkvo3MJ1IWvK6tlfNDw",
   },
 ];
 
